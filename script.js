943601fbba4c8f1d6848676ec41d306915f973f3
imran <87297004+imranshaikh9930@users.noreply.github.com>
2023-11-22 13:37:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+
+const squares = document.querySelectorAll('.square');
+
+  squares.forEach(square => {
+    square.addEventListener('mouseover', () => {
+      squares.forEach(otherSquare => {
+        if (otherSquare !== square) {
+          otherSquare.classList.add('coffee');
+        }
+      });
+    });
+
+    square.addEventListener('mouseout', () => {
+      squares.forEach(otherSquare => {
+        if (otherSquare !== square) {
+          otherSquare.classList.remove('coffee');
+        }
+      });
+    });
+  });
